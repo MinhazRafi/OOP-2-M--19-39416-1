23da183471d1093086eccb6ab256ae05910693d0
Minhazur Rahman <iamrafi404@gmail.com>
2021-01-31 20:24:20 -0800

    Basic Operations

---
+++ startsln/start/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           double x = 36, y = 4;
+           Console.WriteLine("Input please");
+           String name= Console.ReadLine();
+           Console.Write("My name is "+name);
+            
+            Console.WriteLine("\nAddition of {0} and {1} is " +(x+y));
+            Console.WriteLine("Subtraction of x and y is " +(x - y));
+            Console.WriteLine("Multiple of x and y is " +(x*y));
+            Console.WriteLine("division of x and y is " +(x/ y));
+            Console.ReadKey();
+        }
+    }
+}
