8cdf0f7bb89881e526c9f4c5add3f057288c3f81
Minhazur Rahman <iamrafi404@gmail.com>
2021-02-14 21:00:59 -0800

    lab task - 4

---
+++ Array/Array/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5]{1,2,3,4,5};
+            int sum = 0;
+            
+            for(int i=0;i<arr.Length;i++)
+            {
+                sum = sum + arr[i];
+                Console.WriteLine("sum of the array elements is : "+sum);
+                Console.WriteLine();
+            }
+            for(int i=arr.Length;i>0;i--)
+            {
+                Console.WriteLine("Reverse array elements:");
+                Console.WriteLine(arr[i-1]);
+            }
+
+            int[] arr1 = new int[5];
+            Console.WriteLine("Array elements :");
+            for (int i = 0; i < arr1.Length;i++ )
+            {
+                arr1[i] = i;
+                Console.WriteLine(" " + arr[i]);
+            }
+            Console.WriteLine("Copied elements :");
+            for(int i=0;i<arr.Length;i++)
+            {
+                arr[i] = arr1[i];
+                
+                Console.WriteLine(" " + arr1[i]);
+            }
+        }
+    }
+}
---
+++ StaticAndPassValRef/StaticAndPassValRef/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticAndPassValRef
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Student s1 = new Student("Tanvir", "1234");
+            Student s2 = new Student("Sanvir", "3456");
+            Student s3 = new Student("Sanvir", "3456");
+            Student s4 = new Student("Sanvir", "3456");
+            Console.WriteLine(s1.Name + " " +s2.Name+"\n");
+            Console.WriteLine(Student.count);
+        }
+    }
+}
